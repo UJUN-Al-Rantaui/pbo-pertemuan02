b483f1205120fd42785df42de9ee28bae0e2fcfa
UJUN-Al-Rantaui <ujun.muhammadjunaidi@gmail.com>
2024-03-26 14:15:15 +0800

    Memanggil method dari banyak Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa {
+    public static void tampilanInfo() {
+        System.out.println();
+        System.out.println("== Data Mahaiswa =============");
+        System.out.println("NPM : 2210010097");
+        System.out.println("Nama: Muhammad Juanidi");
+        System.out.println("------------------------------");
+    }
+}
